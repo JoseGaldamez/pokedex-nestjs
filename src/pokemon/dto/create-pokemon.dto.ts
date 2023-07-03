6457ad6f103c4f7a519a40610a4b55c52f4da81c
Jose Galdamez <josegaldamez1991@gmail.com>
2023-07-03 15:26:33 -0600

    adding pokemon endpoints

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsString, IsInt, IsPositive, MinLength, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+}
